917df29ef937946c8c7cea016f85d73bd1ea8c25
Cannian <1804781568@qq.com>
2025-10-12 22:42:11 +0800

    123

--- src/config.ts
+++ src/config.ts
@@ -44,11 +44,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		// {
-		// 	name: "GitHub",
-		// 	url: "https://github.com/cannian16", // Internal links should not include the base path, as it is automatically added
-		// 	external: true, // Show an external link icon and will open in a new tab
-		// },
+		{
+			name: "开往",
+			url: "https://www.travellings.cn/go.html", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
