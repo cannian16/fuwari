e98a7ad4d592e539acf31afec85d45a08bd62f1c
cannian <1804781568@qq.com>
2026-03-15 23:11:16 +0800

    修改api的url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.PUBLIC_API_BASE || "https://blog.cannian.space/api";
+const API_BASE =
+	import.meta.env.PUBLIC_API_BASE || "https://blog.cannian.space/api";
 
 const api = axios.create({
 	baseURL: API_BASE,
